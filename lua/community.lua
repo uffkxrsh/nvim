ddfa024add9c31caa43af817dfda86b2411a18b0
uffkxrsh <utkarsh.utee@gmail.com>
2025-07-06 00:56:20 +0530

    initialized

---
+++ lua/community.lua
@@ -0,0 +1,16 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.utility.hover-nvim" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.snippet.nvim-snippets" },
+  { import = "astrocommunity.git.neogit" },
+  -- import/override with your plugins folder
+}
