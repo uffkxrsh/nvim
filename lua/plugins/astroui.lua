396ab4fa07f6694eca774f4a9bddabee1e30f856
uffkxrsh <utkarsh.utee@gmail.com>
2025-07-08 01:07:07 +0530

    added comments in astro ui using avante

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,27 +1,41 @@
--- AstroUI provides the basis for configuring the AstroNvim User Interface
--- Configuration documentation can be found with `:h astroui`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
+-- This file configures the AstroNvim User Interface using the AstroUI plugin.
+-- AstroUI is a core component of AstroNvim that provides a flexible way to customize the look and feel of the editor.
+-- For more information on the available configuration options, see the AstroUI documentation with `:h astroui`.
+--
+-- It is recommended to have the Lua Language Server installed (`:LspInstall lua_ls`)
+-- to get autocompletion and documentation for the configuration options.
 
 ---@type LazySpec
 return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    -- change colorscheme
+    -- Set the colorscheme for the editor.
+    -- This can be any colorscheme that is installed and compatible with AstroNvim.
+    -- For example, you can use "catppuccin", "tokyonight", "onedark", etc.
     colorscheme = "rose-pine-main",
-    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+    -- Customize highlight groups to fine-tune the appearance of different UI elements.
+    -- This allows you to override the default colors of any colorscheme.
     highlights = {
-      init = { -- this table overrides highlights in all themes
+      -- The 'init' table contains highlight overrides that apply to all themes.
+      -- This is useful for setting a consistent style across different colorschemes.
+      init = {
+        -- Example: Uncomment the following line to set the background of normal text to black in all themes.
         -- Normal = { bg = "#000000" },
       },
-      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+      -- You can also create tables for specific themes to apply overrides only when that theme is active.
+      -- For example, the 'astrodark' table here would only apply to the 'astrodark' theme.
+      astrodark = {
+        -- Example: Uncomment the following line to set the background to black only for the 'astrodark' theme.
         -- Normal = { bg = "#000000" },
       },
     },
-    -- Icons can be configured throughout the interface
+    -- Configure the icons used throughout the AstroNvim interface.
+    -- This allows for personalizing the visual elements of the editor.
     icons = {
-      -- configure the loading of the lsp in the status line
+      -- The 'LSPLoading' icons are displayed in the status line to indicate
+      -- that the Language Server Protocol (LSP) is busy.
+      -- These icons create a spinning animation.
       LSPLoading1 = "⠋",
       LSPLoading2 = "⠙",
       LSPLoading3 = "⠹",
